629cb8af78b92ade9f6a69da028592408f6c9d97
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 16:40:54 +0530

    Create Student.java

---
+++ 03_ELAndJSTLTag/src/com/sushant/Student.java
@@ -0,0 +1,38 @@
+package com.sushant;
+
+public class Student {
+
+	private String name;
+	private int age;
+	private String location;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", location=" + location + "]";
+	}
+	
+}
